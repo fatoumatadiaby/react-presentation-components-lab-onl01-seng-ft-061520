5a1df4f57ce25d959d0a6ccec556b0fd0c03f1d5
fatoumatadiaby <fatimad5293@gmail.com>
2020-10-26 17:39:01 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,7 @@
 // Code SimplerComponent Here
+import React from "react";
+import { ProvidePlugin } from "webpack";
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>
+
+export default SimplerComponent;
